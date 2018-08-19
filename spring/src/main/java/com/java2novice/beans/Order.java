22e50782b6229833b0b485c42d8a509f2b8daaa7
alamnr <alamnr@gmail.com>
2018-08-20 00:05:19 +0600

    set spring bean scope  using annotation

--- spring/src/main/java/com/java2novice/beans/Order.java
+++ spring/src/main/java/com/java2novice/beans/Order.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-
+@Lazy
 public class Order {
 	
 	
